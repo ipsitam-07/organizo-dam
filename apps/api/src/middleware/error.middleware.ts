96590925b77ade649ba3ccb09b9bb617b67bb0a4
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-02-26 12:15:27 +0530

    feat: add error middleware

---
+++ apps/api/src/middleware/error.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "@repo/logger";
+import { AppError } from "../utils/error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    logger.warn(
+      `[${req.method}] ${req.path} - ${err.statusCode} - ${err.message}`
+    );
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  // Unhandled/Unexpected Errors
+  logger.error(`[${req.method}] ${req.path} - 500 - ${err.message}`, {
+    stack: err.stack,
+  });
+  res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+};
